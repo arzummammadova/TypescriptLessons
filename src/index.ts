1e7ab5bebe6f26cfa1dcfabdda7da89f3b1ef9dd
arzummammadova <arzuam-af106@code.edu.az>
2025-08-30 13:00:15 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,105 @@
+// let num: number = 2;
+// console.log("Number is:", num);
+
+// let username:string="Arzu"
+// console.log("username is",username)
+// let password:string="2345skkffkfk";
+// console.log("password is",password);
+// let isAdmin:boolean=true;
+// console.log(isAdmin);
+
+// //number string boolean var any var amma tovsiyye ounur
+
+// const num1:any=4;
+// console.log(num1)
+
+
+// let input:unknown="hello"
+// if(typeof input==="string"){
+//     console.log(input.toLocaleUpperCase())
+// }
+// else{
+//     console.log("rewem deyil")
+// }
+
+// let num2: number=2;
+// console.log(num2)
+
+// let num3: void;
+// num3=undefined;
+// console.log(num3)
+
+// function greet():void{
+//     console.log("salam")
+// }
+
+// greet();
+
+// function hi(a:string,b:string):void{
+//     console.log(a+b)
+// }
+// hi( "Salam", " Arzu" )
+
+// function logMessage(msg: string): void {
+//   console.log("Message:", msg);
+// }
+// logMessage( "Hello, TypeScript!" );
+
+// function errormessage():never{
+//     throw new Error("this is error")
+// }
+//  errormessage()
+
+// //  username (string), age (number), isOnline (boolean) dəyişənləri yarat.
+
+// let username1:string="Jobhn"
+
+// console.log(username1)
+// let age:number=20
+// console.log(age)
+// const isOnline:boolean=true
+// console.log(isOnline)
+
+// // any tipli dəyişən yarat → əvvəlcə number, sonra string ver.
+
+// let datas:any=5
+// console.log(datas)
+// datas="salam"
+// console.log(datas)
+
+// // unknown tipli dəyişən yarat → typeof ilə yoxla.
+// let forany:unknown=10
+// if(typeof forany==="number"){
+//     console.log(forany)
+// }
+// else{
+//     console.log("not a number")
+// }
+// console.log(forany)
+
+// // logMessage funksiyası yaz → string qəbul etsin və konsola çıxarsın.
+// function  logMessagee(s:string):void{
+// console.log(s)
+// }
+// logMessagee("salam")
+// // add funksiyası yaz → 2 number alsın və cəmini qaytarsın.
+
+
+// function sum(a:number,b:number): number{
+// return(a+b)
+// }
+// sum(2,3)
+
+// function summ(a: number, b: number): number {
+//     return a + b;
+// }
+
+// console.log("Sum is:", summ(2, 3)); // Sum is: 5
+
+//ders2
+let numbers:number[]=[1,2,3,4,5]
+console.log(numbers)
+
+
+let strings:string[]=["Arzu","Ayla"]
+console.log(strings)
